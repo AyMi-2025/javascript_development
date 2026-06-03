c786f7f89b3035e67db1c4173a09b0946875450c
AyMi@2025 <ayanmaiti558@gmail.com>
2026-06-03 15:13:07 +0530

    javascript phase_01

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log('normal msg')
+console.warn('warning msg')
+console.error('error msg')
+console.table(['Ayan', 18, 'Kolkata'])
+
+// Operations
+
+var a
+a=10
+var b
+b=20
+
+console.log(a,b)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+// Primitive Data Types
+
+//Number
+//Boolean
+//String
+//Undefined
+//Null
+//Symbol
+
+// Non-Primitive Data Types (Reference)
+
+// Object
+// Array
+// Function
+
+
+// Alert , Confirm and Prompt
+
+alert('This is an alert message')
+confirm('Are you sure?')
+prompt('What is your name?')
