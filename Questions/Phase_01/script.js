eacc4e0d0c75706638805666d8295fe1a26e08da
AyMi@2025 <ayanmaiti558@gmail.com>
2026-06-28 10:19:04 +0530

    Phase 01 Questions (Part 01)

---
+++ Questions/Phase_01/script.js
@@ -0,0 +1,117 @@
+/* ==============================================
+            Console & Basics
+   ============================================== */
+// 1. Print "Hello JavaScript" in the console.
+console.log("Hello JavaScript");
+
+// 2. Print your name, age, and city using one console.log().
+console.log("Name:-" , 'Ayan' , "Age:-" ,'18',"City;- ", 'Kolkata');
+
+// 3. Print a warning message using console.warn().
+// alert("This is Warning Msg ......")
+
+// 4. Print an error message using console.error().
+console.error("ERROR MSG..");
+
+// 5. Use console.table() to display an array of 5 numbers.
+let a = [1,2,3,4,5]
+console.log(a)
+
+/* ==============================================
+            Variables
+   ============================================== */
+// 1. Create a variable called `studentName` and store your name in it.
+var studentName = "RAHUL"
+console.log(studentName);
+
+// 2. Create a variable `age` and print it.
+var age = 32
+console.log(age);
+
+// 3. Create two variables and swap their values.
+var b = 3
+var c = 5
+
+console.log(b, c)
+
+var d = c
+var e = b
+console.log(d, e)
+
+// 4. Create a constant variable for `PI` and print it. 
+const f = "PI"
+console.log(f);
+
+// 5. Declare a variable without assigning a value and print it.
+var g
+console.log(g);
+
+// 6. Create a variable `score` and increase it by 10.
+var score = 30
+var n = score + 10
+console.log(n);
+
+// 7. Create three variables for first name, last name, and full name.
+var firstName = "Ayan"
+var lastName = "Maiti"
+var fullName = firstName + " " + lastName
+console.log(fullName);
+
+/* ==============================================
+            Data Types
+   ============================================== */
+// 1. Create variables of type string, number, boolean, null, and undefined.
+var str = "String"
+var num = 2
+var bool = str.startsWith("B")
+var emp = null
+var unde
+
+// 2. Check the type of different variables using `typeof`.
+console.log(typeof(str))
+console.log(typeof(num));
+console.log(typeof(bool));
+console.log(typeof(unde));
+
+// 3. Store your mobile number in a variable and check its type.
+var mobileNumber = 897654529
+console.log(typeof(mobileNumber));
+
+// 4. Create a variable with value `null` and check its type.
+console.log(typeof(emp))
+
+// 5. Create a bigint number and print it.
+var bigin = 61542698746182476376
+console.log(bigin);
+
+
+/* ==============================================
+            Type Conversion & Coercion
+   ============================================== */
+/* ==============================================
+            Operators
+   ============================================== */
+/* ==============================================
+            Strings
+   ============================================== */
+/* ==============================================
+            Numbers & Math
+   ============================================== */
+/* ==============================================
+            Conditionals
+   ============================================== */
+/* ==============================================
+            Truthy & Falsy
+   ============================================== */
+/* ==============================================
+            Ternary Operator
+   ============================================== */
+/* ==============================================
+            Mixed Practice Questions
+   ============================================== */
+/* ==============================================
+            Logical Thinking Questions
+   ============================================== */
+/* ==============================================
+            Challenge Questions for Beginners
+   ============================================== */
